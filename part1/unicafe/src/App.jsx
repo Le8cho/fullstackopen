b4b99003cda357ff1d8a16b322334472e54ebb5c
Le8chito <107323291+Le8cho@users.noreply.github.com>
2026-09-04 11:03:41 -0500

    part1 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,25 +1,31 @@
 import { useState } from 'react'
 
-const Button = ({setFeedBack, feedback, text}) => <button onClick={() => setFeedBack(feedback + 1)}>{text}</button>
+const Button = ({setFeedBack, feedback, text, setAll, all}) => {
 
+  return <button onClick={() => { setFeedBack(feedback + 1); setAll(all+1)}}>{text}</button>
 
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button setFeedBack={setGood} feedback={good} text="good"></Button>
-      <Button setFeedBack={setNeutral} feedback={neutral} text="neutral"></Button>
-      <Button setFeedBack={setBad} feedback={bad} text="bad"></Button>
+      <Button setFeedBack={setGood} feedback={good} text="good" setAll={setAll} all={all}></Button>
+      <Button setFeedBack={setNeutral} feedback={neutral} text="neutral" setAll={setAll} all={all}></Button>
+      <Button setFeedBack={setBad} feedback={bad} text="bad" setAll={setAll} all={all}></Button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
     </div>
   )
 }
