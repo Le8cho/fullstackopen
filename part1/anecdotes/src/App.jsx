9970b0826655d53f9f7251c5ae1f4ff5faa7b835
Le8chito <107323291+Le8cho@users.noreply.github.com>
2026-09-06 12:51:14 -0500

    part1 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,48 +1,69 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  let arrayCount = anecdotes.length
+  let arrayCount = anecdotes.length;
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
 
-  const [votes,setVotes] = useState(Array(arrayCount).fill(0));
+  const [votes, setVotes] = useState(Array(arrayCount).fill(0));
 
-  const handleRandom = (max) =>{
-    let randomItem = Math.floor(Math.random()*max)
-    setSelected(randomItem)
-  }
+  const [maxVoted, setMax] = useState(-1);
 
-  const handleVote = () =>{
-    let copy = [...votes]
-    copy[selected]+=1
-    setVotes(copy)
-    console.log(votes)
-  }
+  const handleRandom = (max) => {
+    let randomItem = Math.floor(Math.random() * max);
+    setSelected(randomItem);
+  };
 
+  const handleVote = () => {
+    let copy = [...votes];
+    let current = maxVoted;
+    if(current === -1){
+      current = 0
+    }
+    copy[selected] += 1;
+    copy.forEach((e, i) => {
+      if (copy[current] < copy[i]) {
+        current = i;
+      }
+    });
+    setMax(current);
+    setVotes(copy);
+  };
   return (
     <div>
       <div>
-      {anecdotes[selected]}
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[selected]}</p>
       </div>
-      <button onClick={()=>{handleVote()}}>
+      <button
+        onClick={() => {
+          handleVote();
+        }}
+      >
         Vote Anecdote
       </button>
-      <button onClick={() => {handleRandom(arrayCount)}}>
+      <button
+        onClick={() => {
+          handleRandom(arrayCount);
+        }}
+      >
         Select Random Anecdote
       </button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[maxVoted]}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
