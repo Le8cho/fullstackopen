b424f2cd2585f00a0eff88999ff002106758c32f
Le8chito <107323291+Le8cho@users.noreply.github.com>
2026-09-06 09:04:20 -0500

    part1 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,20 @@ const Button = ({setFeedBack, feedback, text, setAll, all}) => {
 
 }
 
+const Stadistics = ({good, neutral,  bad, all}) =>{
+
+    return (
+     <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
+    </>
+    )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -20,12 +34,7 @@ const App = () => {
       <Button setFeedBack={setGood} feedback={good} text="good" setAll={setAll} all={all}></Button>
       <Button setFeedBack={setNeutral} feedback={neutral} text="neutral" setAll={setAll} all={all}></Button>
       <Button setFeedBack={setBad} feedback={bad} text="bad" setAll={setAll} all={all}></Button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
+      <Stadistics good={good} neutral={neutral} bad={bad} all={all}></Stadistics>
     </div>
   )
 }
