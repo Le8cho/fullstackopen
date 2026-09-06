9daf6a618dd434964ba539b472e40515523aae05
Le8chito <107323291+Le8cho@users.noreply.github.com>
2026-09-06 09:16:42 -0500

    part1 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,51 +1,89 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Button = ({setFeedBack, feedback, text, setAll, all}) => {
+const Button = ({ setFeedBack, feedback, text, setAll, all }) => {
+  return (
+    <button
+      onClick={() => {
+        setFeedBack(feedback + 1);
+        setAll(all + 1);
+      }}
+    >
+      {text}
+    </button>
+  );
+};
 
-  return <button onClick={() => { setFeedBack(feedback + 1); setAll(all+1)}}>{text}</button>
-
-}
-
-const Stadistics = ({good, neutral,  bad, all}) =>{
-  if(all > 0 ){
-        return (
-     <>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
-    </>
-    )
-  }
-  else{
+const Stadistics = ({ good, neutral, bad, all }) => {
+  
+  let average = (good - bad) / all
+  
+  if (all > 0) {
     return (
-    <>
-    <h1>statistics</h1>
-    <p>No feedback given</p>
-    </>)
-  }
+      <>
 
+        <StatisticsLine text={"good"} value = {good}></StatisticsLine>
+        <StatisticsLine text={"neutral"} value = {neutral}></StatisticsLine>
+        <StatisticsLine text={"bad"} value = {bad}></StatisticsLine>
+        <StatisticsLine text={"average"} value = {average}></StatisticsLine>
+      </>
+    );
+  } else {
+    return (
+      <>
+
+        <p>No feedback given</p>
+      </>
+    );
+  }
+};
+
+const StatisticsLine = ({text, value}) => {
+  return (
+    <p>{text}: {value}</p>
+  )
 }
+
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button setFeedBack={setGood} feedback={good} text="good" setAll={setAll} all={all}></Button>
-      <Button setFeedBack={setNeutral} feedback={neutral} text="neutral" setAll={setAll} all={all}></Button>
-      <Button setFeedBack={setBad} feedback={bad} text="bad" setAll={setAll} all={all}></Button>
-      <Stadistics good={good} neutral={neutral} bad={bad} all={all}></Stadistics>
+      <Button
+        setFeedBack={setGood}
+        feedback={good}
+        text="good"
+        setAll={setAll}
+        all={all}
+      ></Button>
+      <Button
+        setFeedBack={setNeutral}
+        feedback={neutral}
+        text="neutral"
+        setAll={setAll}
+        all={all}
+      ></Button>
+      <Button
+        setFeedBack={setBad}
+        feedback={bad}
+        text="bad"
+        setAll={setAll}
+        all={all}
+      ></Button>
+              <h1>statistics</h1>
+      <Stadistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+      ></Stadistics>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
