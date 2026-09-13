299e9f96925f854a2d567489dc341bd4b510b723
Le8chito <107323291+Le8cho@users.noreply.github.com>
2026-09-13 11:58:56 -0500

    part2 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Filter = ({ nameFilter, setNameFilter, setHasFilter }) => {
   const handleNameFilter = (event) => {
@@ -67,13 +68,7 @@ const Person = ({ name, number }) => <p> {name} {number} </p>
 ;
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [hasFilter, setHasFilter] = useState(false);
   const [nameFilter, setNameFilter] = useState("");
   const [newName, setNewName] = useState("");
@@ -82,6 +77,14 @@ const App = () => {
   let displayPersonsFilter = hasFilter
     ? persons.filter((person) => person.name.toLowerCase().includes(nameFilter))
     : persons;
+
+  useEffect(
+    ()=>{
+      axios
+      .get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data))
+    },
+    [])
 
   return (
     <div>
